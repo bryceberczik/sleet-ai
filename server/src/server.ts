4822d5ba773c25e8f6c6a4c91f3e2287bbd147a2
VanceGoblin <zvkubajak@gmail.com>
2025-02-13 20:57:26 -0600

    Created API Routes

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,13 +1,14 @@
 import express from "express";
 import cors from "cors";
+import routes from "./routes/index";
 
 const app = express();
 const PORT = 3001;
 
 app.use(cors());
 app.use(express.json());
-
 app.use(express.urlencoded({ extended: true }));
+app.use(routes);
 
 app.listen(PORT, () => {
   console.log(`Server running on PORT ${PORT}.`);
