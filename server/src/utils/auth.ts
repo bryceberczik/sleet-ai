622c5240712d18eb5e2ff0046970a68fea295633
VanceGoblin <zvkubajak@gmail.com>
2025-02-14 21:56:03 -0600

    Created signUp Function

--- server/src/utils/auth.ts
+++ server/src/utils/auth.ts
@@ -38,7 +38,7 @@ export const authenticateToken = async (
       secretKey,
       (err: VerifyErrors | null, decoded: JwtPayload | string | undefined) => {
         if (err) {
-          let message = "";
+          let message = "Unknown Token Error";
           if (err instanceof TokenExpiredError) message = "Token is expired.";
           if (err instanceof JsonWebTokenError) message = "Token is invalid.";
           res.status(403).json({ message });
@@ -56,6 +56,60 @@ export const authenticateToken = async (
     );
   } catch (error) {
     console.error("Error authenticating token:", error);
+    res.status(500).json({ message: "Server Error" });
+  }
+};
+
+export const signUp = async (req: Request, res: Response) => {
+  const { username, email, password } = req.body;
+
+  try {
+    const parsedUsername = usernameSchema.safeParse(username);
+    const parsedEmail = emailSchema.safeParse(email);
+    const parsedPassword = passwordSchema.safeParse(password);
+
+    if (
+      !parsedUsername.success ||
+      !parsedEmail.success ||
+      !parsedPassword.success
+    ) {
+      res.status(400).json({ message: "Auth Parsing Error" });
+      return;
+    }
+
+    const existingUser = await prisma.user.findUnique({
+      where: { email: parsedEmail.data },
+    });
+
+    if (existingUser) {
+      res.status(403).json({ message: "User with this email already exists." });
+    }
+
+    const saltRounds = 10;
+    const hashedPassword = await bcrypt.hash(parsedPassword.data, saltRounds);
+
+    const newUser = await prisma.user.create({
+      data: {
+        username: parsedUsername.data,
+        email: parsedEmail.data,
+        password: hashedPassword,
+      },
+      select: {
+        id: true,
+        email: true,
+        hasPremium: true,
+      },
+    });
+
+    const token = jwt.sign(
+      { id: newUser.id, email: newUser.email, hasPremium: newUser.hasPremium },
+      secretKey,
+      { expiresIn: "7d" }
+    );
+
+    res.status(201).json(token);
+  } catch (error) {
+    console.error("Error signing up user:", error);
     res.status(500).json({ message: "Server Error" });
   }
 };
