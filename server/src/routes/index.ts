7c39d36ce92fe31efd824a0b6d8172e0efbe9990
VanceGoblin <zvkubajak@gmail.com>
2025-02-14 22:38:13 -0600

    Added Auth Routes

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,8 +1,15 @@
 import { Router } from "express";
 import apiRouter from "./api/index";
+import authRouter from "./auth/authRoutes";
+import { authenticateToken } from "../utils/auth";
 
 const router = Router();
 
-router.use("/api", apiRouter);
+router.use(
+  "/api",
+  // authenticateToken,
+  apiRouter
+);
+router.use("/auth", authRouter);
 
 export default router;
